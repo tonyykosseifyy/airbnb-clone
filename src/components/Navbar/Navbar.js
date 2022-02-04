97ddb5dd522f1d688bfb9c7f577706402ccc8ba8
tonyykosseifyy <tonykosseify123@gmail.com>
2022-02-05 00:46:21 +0200

    2 files modified

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
-import React from "react" ;
+import React, { useState } from "react" ;
 import "./Navbar.css";
 import { AirbnbButton , UserButton } from "./navbar-components";
 import { FiMenu } from "react-icons/fi" ;
 import { noUserLink } from "../../assets/links";
 
 const Navbar = () => {
+  const [ exp , setExp ] = useState(0);
   return (
     <header className="navbar">
         <div className="navbar-top">
@@ -15,8 +16,8 @@ const Navbar = () => {
             />
           </div>
           <div className="navbar-center">
-            <h4>Places to stay</h4>
-            <h4>Experiences</h4>
+            <h4 className={exp === 0 && "active"} onClick={() => setExp(0)}>Places to stay</h4>
+            <h4 className={exp === 1 && "active"} onClick={() => setExp(1)}>Experiences</h4>
             <h4>Online Experiences</h4>
           </div>
           <div className="navbar-right">
