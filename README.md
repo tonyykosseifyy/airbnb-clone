f4123123c97f00b9af6a9f313f6599f46e13954f
tonyykosseifyy <tonykosseify123@gmail.com>
2022-02-04 22:06:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-nwzwy1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-nwzwy1)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+ function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
+
+
+export default App ;
