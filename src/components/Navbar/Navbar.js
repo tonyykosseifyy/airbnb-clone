d3ceb49e80f9252993c1f668c5e0c80807c4bb9d
tonyykosseifyy <tonykosseify123@gmail.com>
2022-02-04 22:32:18 +0200

    2 files modified

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,15 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <header className="navbar">
-        iaosdn aiosnd aoisnd
+        <div className="navbar-top">
+          <div className="navbar-logo">
+            <img 
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWGgtOwQ63_sPG6c1Z1PGfIxQ5I_rxXhYzBL_9b6meEEZNmlkcL0_dJHT2MZ5X7Ygi_Ow&usqp=CAU"
+              alt="airbnb" 
+            />
+          </div>
+          
+        </div>
     </header>
   );
 };
